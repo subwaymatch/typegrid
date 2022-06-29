2acd6eee63c7bc0931b84e75d9482383693fc481
Ye Joo Park <subwaymatch@gmail.com>
2022-06-28 23:53:10 -0500

    Remove typewritter-effect, upgrade nextjs

--- components/color-pairs/ColorPairBox.tsx
+++ components/color-pairs/ColorPairBox.tsx
@@ -6,6 +6,7 @@ import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
 import Link from "next/link";
 import clsx from "clsx";
 import { Tooltip } from "@mui/material";
+import { AnimatePresence, motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import positiveWords from "data/positive-words";
 import sampleSize from "lodash/sampleSize";
@@ -16,8 +17,42 @@ interface IProps {
   isLastInRow: boolean;
 }
 
+const variants = {
+  enter: {
+    y: -20,
+    opacity: 0,
+  },
+  center: {
+    zIndex: 1,
+    y: 0,
+    opacity: 1,
+  },
+  exit: {
+    zIndex: 0,
+    y: 20,
+    opacity: 0,
+  },
+};
+
 export default function ColorPairBox({ pair, flexBasis, isLastInRow }: IProps) {
   const [isHovering, setIsHovering] = useState<boolean>(false);
+  const [wordList, setWordList] = useState<string[]>([]);
+
+  useEffect(() => {
+    setWordList(sampleSize(positiveWords, 10));
+  }, []);
+
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      let next = index + 1;
+      if (next === wordList.length) {
+        next = 0;
+      }
+      setIndex(next);
+    }, 3 * 1000);
+  }, [index, setIndex, wordList]);
 
   return (
     <div
@@ -28,14 +63,40 @@ export default function ColorPairBox({ pair, flexBasis, isLastInRow }: IProps) {
     >
       <div
         className={styles.preview}
-        onMouseEnter={() => setIsHovering(true)}
-        onMouseLeave={() => setIsHovering(false)}
+        // onMouseEnter={() => setIsHovering(true)}
+        // onMouseLeave={() => setIsHovering(false)}
         style={{
           color: pair.fontColor.hex(),
           backgroundColor: pair.bgColor.hex(),
         }}
       >
-        <p>{isHovering ? "Hovering" : "Text"}</p>
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            position: "absolute",
+          }}
+        >
+          <AnimatePresence>
+            <motion.span
+              style={{ position: "absolute" }}
+              variants={variants}
+              key={index}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              transition={{
+                y: { type: "spring", stiffness: 300, damping: 200 },
+                opacity: { duration: 0.5 },
+              }}
+            >
+              {wordList[index]}
+            </motion.span>
+          </AnimatePresence>
+        </div>
       </div>
 
       <div className={styles.bottomBar}>
